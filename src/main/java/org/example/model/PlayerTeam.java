231e8304455a1a46c90bf4ae450a2ca11e0d0fdf
Mikhail-M-K <mmk_kz@mail.ru>
2023-03-14 12:12:30 +0700

    реализовано: создание игрока,перевод между командами, удаление игрока

--- src/main/java/org/example/model/PlayerTeam.java
+++ src/main/java/org/example/model/PlayerTeam.java
@@ -30,4 +30,71 @@ public class PlayerTeam {
 
     @Column(name="role_in_team")
     private String roleInTeam;
+
+    public PlayerTeam() {
+    }
+
+    public PlayerTeam(Team team, String name, String surname, String patronymic, Date birthdate, String roleInTeam) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.birthdate = birthdate;
+        this.roleInTeam = roleInTeam;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getRoleInTeam() {
+        return roleInTeam;
+    }
+
+    public void setRoleInTeam(String roleInTeam) {
+        this.roleInTeam = roleInTeam;
+    }
 }
--- src/main/java/org/example/service/PlayerTeamServiceImpl.java
+++ src/main/java/org/example/service/PlayerTeamServiceImpl.java
@@ -1,27 +1,59 @@
 package org.example.service;
 
+import org.example.model.PlayerTeam;
 import org.example.model.dto.PlayerTeamDto;
 import org.example.model.dto.PlayerTeamUpdDto;
+import org.example.repos.PlayerTeamRepo;
+import org.example.repos.TeamRepo;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class PlayerTeamServiceImpl implements PlayerTeamService{
 
+    PlayerTeamRepo playerTeamRepo;
+    TeamRepo teamRepo;
+
+    public PlayerTeamServiceImpl(PlayerTeamRepo playerTeamRepo, TeamRepo teamRepo) {
+        this.playerTeamRepo = playerTeamRepo;
+        this.teamRepo = teamRepo;
+    }
+
+    @Transactional
     @Override
     public void createPLayer(PlayerTeamDto playerTeamDto) {
-
+        playerTeamRepo.save(DtoToPlayer(playerTeamDto));
     }
 
+    private PlayerTeam DtoToPlayer(PlayerTeamDto playerTeamDto) {
+        PlayerTeam player = new PlayerTeam();
+        player.setTeam(teamRepo.findById(playerTeamDto.getTeamId()).get());
+        player.setName(playerTeamDto.getName());
+        player.setSurname(playerTeamDto.getSurname());
+        player.setPatronymic(playerTeamDto.getPatronymic());
+        player.setBirthdate(playerTeamDto.getBirthdate());
+        player.setRoleInTeam(player.getRoleInTeam());
+        return player;
+    }
+
+    @Transactional
     @Override
-    public void transferPlayer(Long Id, Long IdTeam) {
+    public void transferPlayer(Long id, Long idTeam) {
+        PlayerTeam playerTeam = playerTeamRepo.getReferenceById(id);
+        playerTeam.setTeam(teamRepo.findById(idTeam).get());
+        playerTeamRepo.save(playerTeam);
 
     }
 
+    @Transactional
     @Override
-    public void deletePlayer(Long Id) {
+    public void deletePlayer(Long id) {
+        playerTeamRepo.deleteById(id);
 
     }
 
+    @Transactional
     @Override
     public void updatePlayer(PlayerTeamUpdDto playerTeamUpdDto, Long id) {
 
